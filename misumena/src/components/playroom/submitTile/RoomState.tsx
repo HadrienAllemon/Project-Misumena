b7ff564cb48e8643790830382433c0518fcd458b
Hadrien Allemon <hadrienallemon@gmail.com>
2026-02-11 09:56:47 +0100

    three dots animation for roomstate

--- misumena/src/components/playroom/submitTile/RoomState.tsx
+++ misumena/src/components/playroom/submitTile/RoomState.tsx
@@ -1,10 +1,41 @@
 import { Box, Text } from "@chakra-ui/react"
+import { useContext } from "react";
+import { SocketContext } from "src/contexts/socket/SocketContext";
 
 export const RoomState = ({ disabled } : {disabled: boolean}) => {
+    const { state, dispatch } = useContext(SocketContext);
+    const currentPlayer = state.room?.users.find(user => user.id === state.room?.currentTurnUserId)
+    const waitToStart = state.room?.roomState === "idle"
+    const ThreeDotAnim = () =>{
+        return (
+            <span className="threeDotAnim"/>
+        )
+    }
+    if (waitToStart) {
+        return (
+            <Box className="roomState">
+                <Text>
+                    Waiting for Admin to start the game 
+                    <ThreeDotAnim />
+                </Text>
+            </Box>
+        )
+    }
 
-    return (
-        <Box className="roomState Sunday">
-            {disabled ? <Text>Waiting for other players...</Text> : <Text>Your turn to submit a word!</Text>}
-        </Box>
-    )
+    if (currentPlayer !== state.currentUser) {
+        return (
+            <Box className="roomState">
+                <Text>
+                    Waiting for {currentPlayer?.name} to submit a word 
+                    <ThreeDotAnim />
+                </Text>
+            </Box>
+        )
+    } else {
+        return (
+            <Box className="roomState">
+                <Text>Your turn to submit a word!</Text>
+            </Box>
+        )
+    }
 }
